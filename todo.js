0b2bf5c058f94936fa3f0dcaa371b98f8027c25f
Sharon Levin <slevin@almodj.com>
2019-03-27 15:09:22 -0400

    add/remove items working

--- todo.js
+++ todo.js
@@ -4,14 +4,16 @@ $("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
-// delete todos
+// delete items
 $("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(400, function(){
     $(this).remove();
   });
+  // stops event bubbling
   event.stopPropagation();
 });
 
+// add items
 $("input[type='text']").keypress(function(event){
   // take item from text input only when user hits enter
   if(event.which === 13){
