c95be1df2d270dd5b25dda4d5d1d84336bcd85c0
shaerins <srcbun@gmail.com>
2019-04-02 11:56:39 -0400

    finish up styling

--- todo.js
+++ todo.js
@@ -19,6 +19,11 @@ $("input[type='text']").keypress(function(event){
   if(event.which === 13){
     var newTodo = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X</span> " + newTodo + "</li>");
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + newTodo + "</li>");
   }
+});
+
+// fade in/out input box
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
 });
